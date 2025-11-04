4d98fefb7bfbf8fd9e7db0ca07dac84a7115394b
tddgit096-pc <>
2025-11-04 10:44:54 -0800

    SDLC proposal and pseudocode

---
+++ main.cpp
@@ -0,0 +1,101 @@
+//headers, map, list, etc
+
+//Consts TOTALYEARS=25, BIRTHCHANCEPREDATORS = ; BIRTHCHANCEPREY = ,MAXAGEPREDATORS, PREDATORBIRTHAGE = 5, MAXHUNGER = 2, 
+
+//predator class
+    //MEMBERS=====
+    //const yearlyFood = 2
+    //const birthChance = 4;
+    //const huntefficiency = % to succeed in a hunt
+    //birthChance
+    //name
+    //age
+    //Disease disease
+    //hunger
+    //METHODS=====
+    //lifetick()
+        //if age>MAXAGEPREDATORS || hunger>=MAXHUNGER
+            //die
+        //resolveDisease()
+        //if age>PREDATORBIRTHAGE && !hunger
+            //if rand() < birthchance
+    //die
+    //birth
+        //if rand()%100 < birthChance() && hunger==0
+            //new Predator
+    //hunt(&list)
+        //auto it = list.begin(), list.end()
+            //int currentfood = 1
+            // if (rand()%100 <huntefficiency)
+                //if it->disease
+                    //disease = it->disease
+                //delete element from list
+                //currentfood++
+            //if currentfood==yearlyfood
+                //hunger--
+                //return
+        // else
+            //hunger++
+    //resolveDisease()
+        //if disease
+            //if disease.currentDuration >= disease.maxDuration
+                //delete disease
+                //disease = null
+            //else 
+                //resolveDisease
+        //if rand()%100+1 < lethalityPredatorChance
+            //die
+        
+//prey class
+    //MEMBERS=====
+    //name
+    //age
+    //Disease disease
+    //METHODS=====
+    //die
+    //birth
+    //resolveDisease
+//disease class
+    //name
+    //infectionChance:
+    //currentDuration
+    //maxDuration
+    //lethalityPreyChance
+    //lethalityPredatorChance
+    //METHODS=====
+    //infectRandomPopulation(&list)
+        //if rand() > infectionChance   
+            //return //no one was infected by this disease this year
+        //for each prey in list.
+            //prey.setDisease(this)
+
+//RunSimluation(&map)
+    //print(map)
+    //int i = 1
+    //while(<TOTALYEARS)
+        //For all diseases:
+            //disease.infectRandomPopulation(list of prey)
+        //For all predators:
+            //predator.hunt(list of prey)
+        //for all prey
+            //prey.lifetick
+            //birthChance
+        //for all predators
+            //predator.lifetick
+            //birthChance
+
+        //print(map)
+
+//print(map)
+    //iterate through, print every element. neatly organized
+
+//main
+    //map containing {Coyotes, Lions, Orcas}
+        //3 arrays of lists. List 1 contains members of predator population, with a name, their age, whether they are sick, and when they have last eaten.
+        //list 2 conatins members of the prey population, with a name, with an age, and whether they have a disease.
+        //list 3 contains diseases, which may infect and decrease the predator population
+    //read from data to enter the population of predators, prey, and disease vectors.
+        //populate predators by name and age. hunger starts at 0
+        //populate prey by name and age.
+        //populate diseaseVectors
+    //RunSimluation(&map)
