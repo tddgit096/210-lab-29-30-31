9f0729a7ea8eae551f41bacbf65607bf541472be
tddgit096-pc <>
2025-11-07 18:05:11 -0800

    more progress on diseases vector and hunt.

--- animal.h
+++ animal.h
@@ -4,6 +4,7 @@
 #define ANIMAL_H
 #include <iostream>
 #include <vector>
+#include <disease.h>
 using namespace std;
 
 const int HUNTATTEMPTSPERYEAR = 2, HUNTSUCCESSCHANCE = 10, TRANSMISSIONCHANCE = 50;
@@ -21,13 +22,14 @@ public:
     string genRandName();
     void lifetick();
     void hunt(list<animal> &p);
+    void die();
     // Constructors
     animal()                          { name = genRandName(); age = 0; }
     animal(bool ispred)               { name = genRandName(); age = 0; isPredator=ispred;}
     animal(string name)               { set_name(name);set_age(0);isPredator=false;}
     animal(string name, bool ispred)  { set_name(name);set_age(0); isPredator=ispred;}
     animal(string name,int age, bool ispredator = false)               { set_name(name);set_age(age);isPredator = ispredator;}
-
+    ~animal(); 
 
     // setters and getters
     void set_name(string n)         { name = n; };
@@ -47,18 +49,18 @@ public:
                 auto it = p.begin();    //make an iterator at start of the list
                 advance(it,index);       //advance to the index we gen'd
                 if(it->getDisease().size()>0){ //target is sick
-                    for (vector<disease*>::iterator diseaseIt = (it->getDisease()).begin(); diseaseIt != (it->getDisease()).end(); ++diseaseIt) {
+                    for (auto diseaseIt = (it->getDisease()).begin(); diseaseIt != (it->getDisease()).end(); ++diseaseIt) {
                         if(rand()%100<TRANSMISSIONCHANCE)
-                           setDisease(*diseaseIt->first); 
+                           setDisease(*diseaseIt); 
                     }
-                    TRANSMISSIONCHANCE   
                 }
+                it->die();
             }
         }
     }
 
     void lifetick(){
-
+        //TODO
     }
 
     string genRandName(){
@@ -75,15 +77,28 @@ public:
         return name;
     }
     void print(){
-        cout<<"["<<get_name()<<" : AGE:"<<get_age<<" : ";
-        if(DiseaseHolder!=NULL){
-            disease* D = getDisease();
-            cout<<D->get_name();
+        cout<<"["<<get_name()<<" / AGE:"<<get_age<<" : ";
+        if(DiseaseHolder.size()>0){
+            cout<<"DISEASES: ";
+            for (disease * d : DiseaseHolder) {
+                cout<<d->get_name()<<" ";
+            }
         }
-        else
-            cout<<" - ";
         cout<<"]";
     }
-};
+    
+    void die(){
+        delete this;
+    }
+
+    ~animal(){
+        //delete all pointers for diseases vector
+        for (disease* d : DiseaseHolder) {
+            delete d;
+        }
+        //clear the pointers in the vector.
+        DiseaseHolder.clear();
+    }
+}
 
 #endif
